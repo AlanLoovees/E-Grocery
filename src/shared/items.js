9cbe10b46aa2699ba96681602b1784e738609910
Alan Loovees <alanloovees@gmail.com>
2020-08-29 20:24:40 +0530

    data set

---
+++ src/shared/items.js
@@ -0,0 +1,546 @@
+export const ITEMS = [
+    {
+        id: 0,
+        name: 'Orange',
+        image: '/assets/images/item.png',
+        category: 'Fruits & Vegetables',
+        price: '₹50/kg',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 1,
+        name: 'Toothbrush',
+        image: '/assets/images/brush.png',
+        category: 'Daily Needs',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: 'Orange',
+        image: '/assets/images/cleaning.png',
+        category: 'Cleaning & Household',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 3,
+        name: 'Pen',
+        image: '/assets/images/pen.png',
+        category: 'Stationary',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 4,
+        name: 'Orange',
+        image: '/assets/images/item.png',
+        category: 'Fruits & Vegetables',
+        price: '₹50/kg',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 5,
+        name: 'Orange',
+        image: '/assets/images/item.png',
+        category: 'Fruits & Vegetables',
+        price: '₹50/kg',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 6,
+        name: 'Orange',
+        image: '/assets/images/item.png',
+        category: 'Fruits & Vegetables',
+        price: '₹50/kg',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 7,
+        name: 'Orange',
+        image: '/assets/images/item.png',
+        category: 'Fruits & Vegetables',
+        price: '₹50/kg',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 8,
+        name: 'Orange',
+        image: '/assets/images/item.png',
+        category: 'Fruits & Vegetables',
+        price: '₹50/kg',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 9,
+        name: 'Orange',
+        image: '/assets/images/item.png',
+        category: 'Fruits & Vegetables',
+        price: '₹50/kg',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 10,
+        name: 'Orange',
+        image: '/assets/images/item.png',
+        category: 'Fruits & Vegetables',
+        price: '₹50/kg',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 11,
+        name: 'Toothbrush',
+        image: '/assets/images/brush.png',
+        category: 'Daily Needs',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 12,
+        name: 'Toothbrush',
+        image: '/assets/images/brush.png',
+        category: 'Daily Needs',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 13,
+        name: 'Toothbrush',
+        image: '/assets/images/brush.png',
+        category: 'Daily Needs',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 14,
+        name: 'Toothbrush',
+        image: '/assets/images/brush.png',
+        category: 'Daily Needs',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 15,
+        name: 'Toothbrush',
+        image: '/assets/images/brush.png',
+        category: 'Daily Needs',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 16,
+        name: 'Toothbrush',
+        image: '/assets/images/brush.png',
+        category: 'Daily Needs',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 17,
+        name: 'Toothbrush',
+        image: '/assets/images/brush.png',
+        category: 'Daily Needs',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 18,
+        name: 'Cleaner',
+        image: '/assets/images/cleaning.png',
+        category: 'Cleaning & Household',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 19,
+        name: 'Cleaner',
+        image: '/assets/images/cleaning.png',
+        category: 'Cleaning & Household',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 20,
+        name: 'Cleaner',
+        image: '/assets/images/cleaning.png',
+        category: 'Cleaning & Household',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 21,
+        name: 'Cleaner',
+        image: '/assets/images/cleaning.png',
+        category: 'Cleaning & Household',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 22,
+        name: 'Cleaner',
+        image: '/assets/images/cleaning.png',
+        category: 'Cleaning & Household',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 23,
+        name: 'Cleaner',
+        image: '/assets/images/cleaning.png',
+        category: 'Cleaning & Household',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 24,
+        name: 'Cleaner',
+        image: '/assets/images/cleaning.png',
+        category: 'Cleaning & Household',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 25,
+        name: 'Pen',
+        image: '/assets/images/pen.png',
+        category: 'Stationary',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 26,
+        name: 'Pen',
+        image: '/assets/images/pen.png',
+        category: 'Stationary',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 27,
+        name: 'Pen',
+        image: '/assets/images/pen.png',
+        category: 'Stationary',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 28,
+        name: 'Pen',
+        image: '/assets/images/pen.png',
+        category: 'Stationary',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 29,
+        name: 'Pen',
+        image: '/assets/images/pen.png',
+        category: 'Stationary',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 30,
+        name: 'Pen',
+        image: '/assets/images/pen.png',
+        category: 'Stationary',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    },
+    {
+        id: 31,
+        name: 'Pen',
+        image: '/assets/images/pen.png',
+        category: 'Stationary',
+        price: '₹50',
+        shops: [
+            {
+                shopID: 0,
+                name: 'Shop 0'
+            },
+            {
+                shopID: 1,
+                name: 'Shop 1'
+            },
+        ]
+    }
+];
